d84a97d9d8960379dd24ba0ba0d64b77266c570e
Dana <dana.xfc@gmail.com>
2026-05-29 00:01:02 -0600

    creación de interfaz torreta

---
+++ src/main/java/frc/robot/Interfaces/TurretIO.java
@@ -0,0 +1,43 @@
+package frc.robot.Interfaces;
+
+import com.stzteam.features.marsprocessor.Fallback;
+import com.stzteam.mars.models.singlemodule.Data;
+import com.stzteam.mars.models.singlemodule.IO;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+@Fallback
+public interface TurretIO extends IO<TurretIO.TurretInputs> {
+
+    public class TurretInputs extends Data<TurretInputs>{
+        
+        public Rotation2d currentAngle;
+        public Rotation2d targetAngle;
+        public double velocityRPS = 0.0;
+        public double appliedVolts = 0.0;
+        public double current = 0.0;
+
+        public TurretInputs snapshot() {
+            TurretInputs copy = new TurretInputs();
+
+            copy.appliedVolts = appliedVolts;
+            copy.current = current;
+
+            return copy;
+        }
+
+    }
+
+    public void setVoltage(double appliedVolts);
+
+    public void setPosition(Rotation2d targetAngle);
+
+    public void setPositionWithFF(Rotation2d targetAngle, double velocityRPS);
+
+    public void setSpeed(double speed);
+
+    public void stop();
+
+    public void resetEnc();
+    
+}
